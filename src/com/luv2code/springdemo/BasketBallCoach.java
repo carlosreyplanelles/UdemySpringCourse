5ae34f1ba2ac7f332f0b0bdd1f084731e4cd8902
carlosreyplanelles@gmail.com <carlosreyplanelles@gmail.com>
2018-05-27 20:22:28 +0200

    Inversion of control-XML Configuration Exercise

---
+++ src/com/luv2code/springdemo/BasketBallCoach.java
@@ -0,0 +1,10 @@
+package com.luv2code.springdemo;
+
+public class BasketBallCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Improve your aim on 3 points shots";
+	}
+
+}
