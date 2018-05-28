5604ec1f1650649b42f1bf7ea74a946e54d66fa1
Carlos Rey Planelles <crey@gfi.es>
2018-05-28 13:45:57 +0200

    Dependency injection samples

--- src/com/luv2code/springdemo/BasketBallCoach.java
+++ src/com/luv2code/springdemo/BasketBallCoach.java
@@ -1,6 +1,12 @@
 package com.luv2code.springdemo;
 
 public class BasketBallCoach implements Coach {
+	
+	private FortuneService fortuneService;
+	
+	public BasketBallCoach(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
 
 	@Override
 	public String getDailyWorkout() {
