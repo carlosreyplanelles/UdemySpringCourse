d4c941a7abfa0b48fe72cecd23261d76abc8f40c
darcarlos <carlosreyplanelles@gmail.com>
2018-08-20 20:23:22 +0200

    msg

--- spring-demo-annotations/src/com/luv2code/springdemo/SwimCoach.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/SwimCoach.java
@@ -1,17 +1,39 @@
 package com.luv2code.springdemo;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public class SwimCoach implements Coach {
+	
+	private FortuneService fortuneService;
+	
+	@Value("${foo.email}")
+	private String email;
+	
+	@Value("${foo.team}")
+	private String team;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public SwimCoach(FortuneService happyFortuneService) {
+		fortuneService  = happyFortuneService;
+	}
 
 	@Override
 	public String getDailyWorkout() {
-		// TODO Auto-generated method stub
-		return null;
+		return "Swim 1000 metres";
 	}
 
 	@Override
 	public String getDailyFortune() {
-		// TODO Auto-generated method stub
-		return null;
+		return fortuneService.getFortune();
 	}
 
 }
