4a0fd5a59c035ad05ff364516c9c5a1735e9048d
Carlos Rey Planelles <carlos.rey@itbo-ericsson.com>
2018-09-06 09:26:00 +0200

    Repository architecture change

--- spring-demo-annotations/src/com/luv2code/springdemo/FortuneService.java
+++
@@ -1,9 +0,0 @@
-package com.luv2code.springdemo;
-
-import org.springframework.beans.factory.annotation.Qualifier;
-
-@Qualifier
-public interface FortuneService {
-	
-	public String getFortune();
-}
