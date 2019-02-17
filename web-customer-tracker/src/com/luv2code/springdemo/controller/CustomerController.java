37de24eacbc175ff8502308ea94c259213ef2d15
darcarlos <carlosreyplanelles@gmail.com>
2019-02-17 23:46:15 +0100

    Section 28 - Build a Database Web App - Spring MVC and Hibernate Project - Part 1

---
+++ web-customer-tracker/src/com/luv2code/springdemo/controller/CustomerController.java
@@ -0,0 +1,16 @@
+package com.luv2code.springdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+	
+	@RequestMapping("/")
+	public String listCustomer(Model theModel) {
+		return "list-customers";
+	}
+
+}
