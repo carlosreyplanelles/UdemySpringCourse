18aee8bd7893ec71ae5d9706738fff60450c805e
carlosreyplanelles <carlosreyplanelles@gmail.com>
2019-03-13 20:44:07 +0100

    Section 42 - AOP: @AfterReturning Advice Type

---
+++ spring-demo-aop-read-joinpoint-after-returning/src/com/luv2code/aopdemo/aspect/APIAnalyticsAspect.java
@@ -0,0 +1,20 @@
+package com.luv2code.aopdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Order(3)
+public class APIAnalyticsAspect {
+	
+	
+	
+	@Before("com.luv2code.aopdemo.aspect.AOPExpressions.DAOPackage()")
+	public void APIAnalyticsAdvice() {
+		System.out.println(getClass()+": API Analytics advice");
+	}
+}
