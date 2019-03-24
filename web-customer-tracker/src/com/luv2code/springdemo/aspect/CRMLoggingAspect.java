e37a865daa1790f488f179564c9fd4266e8675a6
darcarlos <carlosreyplanelles@gmail.com>
2019-03-24 20:39:55 +0100

    Section 47 - Spring Security - Getting Started

--- web-customer-tracker/src/com/luv2code/springdemo/aspect/CRMLoggingAspect.java
+++ web-customer-tracker/src/com/luv2code/springdemo/aspect/CRMLoggingAspect.java
@@ -12,40 +12,47 @@ import org.springframework.stereotype.Component;
 
 import com.luv2code.springdemo.entity.Customer;
 
-
-
 @Aspect
 @Component
 public class CRMLoggingAspect {
-	
+
 	private Logger myLogger = Logger.getLogger(getClass().getName());
-	
+
 	@Pointcut("execution(* com.luv2code.springdemo.controller.*.*(..))")
-	private void controllerPackage() {}
+	private void controllerPackage() {
+	}
+
 	@Pointcut("execution(* com.luv2code.springdemo.service.*.*(..))")
-	private void servicePackage() {}
+	private void servicePackage() {
+	}
+
 	@Pointcut("execution(* com.luv2code.springdemo.dao.*.*(..))")
-	private void daoPackage() {}
+	private void daoPackage() {
+	}
+
 	@Pointcut("controllerPackage() || servicePackage() || daoPackage()")
-	private void appFlow() {}
-	
+	private void appFlow() {
+	}
+
 	@Before("appFlow()")
 	public void before(JoinPoint joinpoint) {
 		String method = joinpoint.getSignature().toShortString();
-		myLogger.info("["+ getClass()+ "] =====> "+ method);
+		myLogger.info("[" + getClass() + "] =====> " + method);
 		Object[] args = joinpoint.getArgs();
-		
-		if(args.length > 0) {
-			for (Object arg: args) {
+
+		if (args.length > 0) {
+			for (Object arg : args) {
 				myLogger.info(arg.toString());
 			}
 		}
 	}
-	@AfterReturning(pointcut= "appFlow()", returning="customers")
+
+	@AfterReturning(pointcut = "appFlow()", 
+			returning = "customers")
 	public void afterReturning(JoinPoint joinpoint, List<Customer> customers) {
 		String method = joinpoint.getSignature().toShortString();
-		myLogger.info("[" + getClass() + "] =====> "+ method);
-		myLogger.info("[" + getClass() + "] =====> return: "+ customers);
+		myLogger.info("[" + getClass() + "] =====> " + method);
+		myLogger.info("[" + getClass() + "] =====> return: " + customers);
 	}
 
 }
